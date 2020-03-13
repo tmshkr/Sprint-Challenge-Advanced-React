5caf018753aaffe1712d3ad8f793f8bd14d2e3b8
Tim Shaker <tim@tmshkr.com>
2020-03-13 09:33:28 -0700

    dark mode tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  render(<App />);
+});
+
+it("toggles dark mode", () => {
+  const { getByText } = render(<App />);
+
+  // toggle dark mode on
+  fireEvent.click(getByText(/mode/i));
+  expect(document.body.className).toBe("dark-mode");
+
+  // toggle dark mode off
+  fireEvent.click(getByText(/mode/i));
+  expect(document.body.className).toBe("");
 });
