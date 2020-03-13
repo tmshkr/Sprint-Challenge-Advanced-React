b1fd50acac983265bacace3274c84a07d5ea2fb4
Tim Shaker <tim@tmshkr.com>
2020-03-13 09:52:51 -0700

    PlayerTable test columns

--- client/src/components/PlayerTable.test.js
+++ client/src/components/PlayerTable.test.js
@@ -17,3 +17,14 @@ it("displays rows in tbody", async () => {
     expect(document.querySelectorAll("tbody > tr").length).toBeGreaterThan(0)
   );
 });
+
+it("displays 3 columns in each row of tbody", async () => {
+  render(<PlayerTable />);
+  await wait(() => {
+    const rows = document.querySelectorAll("tbody > tr");
+    expect(rows.length).toBeGreaterThan(0);
+    rows.forEach(r => {
+      expect(r.children.length).toBe(3);
+    });
+  });
+});
