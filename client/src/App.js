6ff395ffbbf9f150fe83668508b56c1c9146d9f7
Tim Shaker <tim@tmshkr.com>
2020-03-13 09:32:55 -0700

    include button to useDarkMode

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Button } from "reactstrap";
 
+import { useDarkMode } from "./hooks/useDarkMode";
 import PlayerTable from "./components/PlayerTable";
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode();
   return (
     <main className="App">
+      <header>
+        <Button color="secondary" onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </Button>
+      </header>
       <PlayerTable />
     </main>
   );
