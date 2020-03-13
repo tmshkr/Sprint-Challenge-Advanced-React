0dd4df027fd50d4f4db50ab42e50272e766f463b
Tim Shaker <tim@tmshkr.com>
2020-03-13 08:37:52 -0700

    render table from data

--- client/src/components/PlayerTable.js
+++ client/src/components/PlayerTable.js
@@ -1,37 +1,47 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { Table } from "reactstrap";
 
 class PlayerTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    this.__isMounted = true;
+    axios.get("http://localhost:5000/api/players").then(({ data }) => {
+      if (this.__isMounted) {
+        this.setState({ players: data });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    // to prevent updating unmounted component
+    this.__isMounted = false;
+  }
+
   render() {
     return (
       <Table>
         <thead>
           <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Search</th>
+            <th>Name</th>
+            <th>Country</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {this.state.players.map(p => (
+            <tr key={p.id}>
+              <th scope="row">{p.searches}</th>
+              <td>{p.name}</td>
+              <td>{p.country}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     );
--- client/src/components/PlayerTable.test.js
+++ client/src/components/PlayerTable.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, wait } from "@testing-library/react";
 import PlayerTable from "./PlayerTable";
 
 it("renders without crashing", () => {
@@ -9,4 +9,11 @@ it("renders without crashing", () => {
 it("displays a table", () => {
   render(<PlayerTable />);
   expect(document.querySelector("table")).toBeTruthy();
+});
+
+it("displays rows in tbody", async () => {
+  render(<PlayerTable />);
+  await wait(() =>
+    expect(document.querySelectorAll("tbody > tr").length).toBeGreaterThan(0)
+  );
 });
